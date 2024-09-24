f4df03391e5ddce4e1f695204cba5368dab34a3e
Luis <luisirk.montoto@gmail.com>
2024-09-24 12:01:56 +0200

    Green: expect FizzFuzz when expression is multiple of 3 and 5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 
 export function add (expression: string) {
     let number = Number(expression);
+    if (number %3 === 0 && number %5 ===0) {
+        return 'FizzFuzz'
+    }
     if (number %3 === 0) {
         return 'Fizz'
     }
