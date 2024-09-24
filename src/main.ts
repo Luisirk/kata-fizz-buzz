30ed6e82a0b3fc6cccfe0cad5dc54dff547831d0
Luis <luisirk.montoto@gmail.com>
2024-09-24 11:23:43 +0200

    green : expect FizzFuzz for numbers multiple of 3 and 5

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ export function add (expression: string) {
 
   let number = Number(expression);
 
+  if( number % 3 === 0 && number % 5 === 0){
+    return 'FizzBuzz';
+  }
   if( number % 3 === 0){
     return 'Fizz';
   }
   if( number % 5 === 0){
     return 'Buzz';
   }
+
 
   return expression;
 
